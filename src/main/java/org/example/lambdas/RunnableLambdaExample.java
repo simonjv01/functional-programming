7797f61c42998ea9d162064894bde8f4d8b830d1
Simon Vargas <sjvargas01@gmail.com>
2023-07-10 19:24:50 -0400

    4 examples of lambda using runnable

--- src/main/java/org/example/lambdas/RunnableLambdaExample.java
+++ src/main/java/org/example/lambdas/RunnableLambdaExample.java
@@ -14,5 +14,23 @@ public class RunnableLambdaExample {
                 System.out.println("Inside Runnable 1");
             }
         };
+
+        new Thread(runnable).start();
+
+        /**
+         * after Java 8 lambda
+         * ()->();
+         */
+
+        Runnable runnableLambda = () -> {
+            System.out.println("Inside Runnable 2");
+        };
+
+        Runnable runnableLambda1 = () -> System.out.println("Inside Runnable 3");
+
+        new Thread(runnableLambda).start();
+        new Thread(runnableLambda1).start();
+
+        new Thread(() -> System.out.println("Runnable 4")).start();
     }
 }
